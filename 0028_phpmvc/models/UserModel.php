1249e03d20f7dbce1caa72f5bc0f7ea237c69c91
ArifMufti7 <arifpemalang99@gmail.com>
2024-10-27 17:18:16 +0700

    Add files via upload

---
+++ 0028_phpmvc/models/UserModel.php
@@ -0,0 +1,22 @@
+<?php
+// models/UserModel.php
+class UserModel {
+    private $db;
+
+    public function __construct($db) {
+        $this->db = $db;
+    }
+
+    public function getAllUsers() {
+        $stmt = $this->db->query("SELECT * FROM users");
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function getUserById($id) {
+        $stmt = $this->db->prepare("SELECT * FROM users WHERE id = :id");
+        $stmt->bindParam(':id', $id);
+        $stmt->execute();
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+}
+?>
